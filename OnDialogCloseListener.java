c9450dcd5f9c774cce35e81a2fc418b4639f177c
lim0434 <chienhuilim0434@gmail.com>
2025-11-01 22:18:08 +0800

    Add files via upload

---
+++ OnDialogCloseListener.java
@@ -0,0 +1,8 @@
+
+package com.example.a202sgitodoapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDialogClose(DialogInterface dialogInterface);
+}
